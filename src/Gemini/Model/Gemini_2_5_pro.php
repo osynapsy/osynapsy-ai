9e7ff967c8d4dc69dc9c982369cf1094f76e4137
Pietro Celeste <p.celeste@osynapsy.net>
2025-12-31 21:13:16 +0100

    Improve code

---
+++ src/Gemini/Model/Gemini_2_5_pro.php
@@ -0,0 +1,11 @@
+<?php
+namespace Osynapsy\AI\Gemini\Model;
+
+/**
+ * Description of Gemini_2_5_Pro
+ *
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
+ */
+class Gemini_2_5_Pro {
+    //put your code here
+}
